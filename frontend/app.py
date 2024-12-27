28a08875fc01f0a67efb0aeaa700de84a04626af
moritalous <moritalous@gmail.com>
2024-12-27 16:20:24 +0000

    フロントエンドのサンプルを追加

---
+++ frontend/app.py
@@ -0,0 +1,43 @@
+import datetime
+
+import boto3
+import streamlit as st
+from botocore.config import Config
+
+st.title("Nova Researcher")
+
+with st.form("form"):
+    flow_identifier = st.text_input("10桁のPrompt Flow IDを入力")
+    task = st.text_input("リサーチしたい内容を入力")
+
+    submit = st.form_submit_button("実行")
+
+
+if submit:
+    config = Config(read_timeout=600)
+    client = boto3.client("bedrock-agent-runtime", config=config)
+
+    with st.spinner("処理中... 5分ほどお待ち下さい"):
+
+        response = client.invoke_flow(
+            flowIdentifier=flow_identifier,
+            flowAliasIdentifier="TSTALIASID",
+            inputs=[
+                {
+                    "content": {
+                        "document": {
+                            "task": task,
+                            "now": datetime.datetime.now().strftime(
+                                "%Y/%m/%d %H:%M:%S"
+                            ),
+                        }
+                    },
+                    "nodeName": "FlowInputNode",
+                    "nodeOutputName": "document",
+                },
+            ],
+        )
+
+        for stream in response["responseStream"]:
+            if "flowOutputEvent" in stream:
+                st.write(stream["flowOutputEvent"]["content"]["document"])
